0d5e834b59702bfd8f1c0f5c5da5d5bfe1d872d8
Zanidean` <`AZanidean@mhc.ab.ca`>
2017-10-25 15:36:22 -0600

    Update Look

--- server.R
+++ server.R
@@ -1,6 +1,8 @@
 library(shiny)
 library(devtools)
 devtools::install_github("zanidean/sra", quick = T)
+devtools::install_github("daattali/colourpicker")
+library(colourpicker)
 library(sRa)
 library(ggrepel)
 library(tidyverse)
@@ -13,9 +15,10 @@ shinyServer(function(input, output) {
     if (is.null(inFile))
       return(NULL)
     
-
-    dataframe <- read.csv(inFile$datapath)
-
+    if(input$type == ".xlsx"){
+    dataframe <- read_excel(inFile$datapath)
+    } else {dataframe <- read.csv(inFile$datapath)}
+    
     dataframe <- dataframe %>% 
       xmR(., "Measure", recalc = T) %>% 
       select(-Order)
@@ -24,48 +27,58 @@ shinyServer(function(input, output) {
   
   
   chart <- reactive({
+    
+    if(input$center == "Center"){alig = 0.5} else {alig = 0}
+    if(input$grid == T){gridcol = "#f2f2f2"} else {gridcol = "#ffffff"}
+    
     whitetheme <- theme_bw() +
       theme(strip.background = element_rect(fill = NA, linetype = 0),
             panel.border = element_rect(color = NA),
-            panel.spacing.y  = unit(5.5, "lines"),
-            panel.spacing.x  = unit(6, "lines"),
+            panel.spacing.y  = unit(10, "lines"),
+            panel.spacing.x  = unit(10, "lines"),
             panel.grid.minor = element_blank(),
-            panel.grid.major = element_line(colour = "#fff2ff"),
+            panel.grid.major = element_line(colour = gridcol),
             axis.ticks.y = element_blank(),
             axis.ticks.x = element_blank(),
             text = element_text(family = "sans"),
-            axis.text.x = element_text(colour = "#000000", size = 13),
+            axis.text.x = element_text(colour = "#000000", size = input$textsize-3),
             axis.title.x = element_text(size = input$textsize, face = "bold"),
             axis.text.y = element_blank(),
             axis.title.y = element_text(size = input$textsize, face = "bold"),
             strip.text = element_text(size = input$titlesize),
             plot.title = element_text(size = input$titlesize, 
-                                      hjust = 0.5, face = "bold"))
+                                      hjust = alig, 
+                                      face = "bold"),
+            plot.subtitle = element_text(size = input$titlesize*.66, 
+                                         hjust = alig))
     
     inFile <- input$inputFile
     if (is.null(inFile))
       return(NULL)
     
     
-    dataframe <- read.csv(inFile$datapath)
+    if(input$type == ".xlsx"){
+      dataframe <- read_excel(inFile$datapath)
+    } else {dataframe <- read.csv(inFile$datapath)}
+    
     
     dataframe <- dataframe %>% 
       xmR(., "Measure", recalc = T) %>% 
       select(-Order)
     dataframe
     
-    ggplot(dataframe, aes(as.character(Time), group = 1)) +
+    plot <- ggplot(dataframe, aes(as.character(Time), group = 1)) +
       geom_line(aes(y = `Central Line`),
                 size = input$refline, 
                 linetype = "dotted", 
                 na.rm = T) +
       geom_line(aes(y = `Lower Natural Process Limit`),  
-                color = "#d02b27",
+                color = input$linecol,
                 size  = input$refline,
                 linetype = "dashed", 
                 na.rm  = T) +
       geom_line(aes(y  = `Upper Natural Process Limit`), 
-                color  = "#d02b27",
+                color  = input$linecol,
                 size   = input$refline, 
                 linetype = "dashed", 
                 na.rm  = T) +
@@ -75,13 +88,24 @@ shinyServer(function(input, output) {
                  color = "#000000") +
       geom_point(aes(y = Measure), 
                  size  = input$dotsize, 
-                 color = "#7ECBB5") + 
-      geom_label_repel(aes(y = Measure, label = Measure),
-                       size = input$labelsize,
-                       label.r = unit(0.2, "lines"),
-                       label.size = 0.15, nudge_x = 0.32) +
+                 color = input$dotcol) + 
+      scale_x_discrete() + 
       guides(colour = FALSE) + whitetheme +
-      labs(x = input$x, y = input$y) + ggtitle(input$title)
+      labs(x = input$x, y = input$y) + ggtitle(input$title, subtitle = input$subtitle)
+    
+      if(input$labelsize != 0){
+        plot <- plot + geom_label_repel(aes(y = Measure, 
+                                                label = if(input$l_format == "Percent") {
+                                                  scales::percent(round(Measure, input$digits))
+                                                } else if(input$l_format == "Dollars"){
+                                                  scales::dollar(round(Measure, input$digits))
+                                                } else {round(Measure, input$digits)}),
+        size = input$labelsize,
+        label.r = unit(0.2, "lines"),
+        label.size = 0.15, nudge_x = 0.32)
+      } 
+    plot
+    
   })
   
 "P:/xmr_generator/"
@@ -115,7 +139,7 @@ shinyServer(function(input, output) {
     content = function(file){
       ggsave(file, chart(),
              height = input$height,
-             width = input$width, dpi = 1200)
+             width = input$width, dpi = input$dpi)
     }
   )
   
--- ui.R
+++ ui.R
@@ -1,76 +1,124 @@
 library(shiny)
 library(sRa)
 library(tidyverse)
+library(readxl)
+library(colourpicker)
 
 shinyUI(fluidPage(theme = "www/paper_modified.css",
-                  tags$style(HTML(".js-irs-0 .irs-single, .js-irs-0 .irs-bar-edge, 
-                  .js-irs-0 .irs-bar {color: #000; background: #7ECBB5 ; border-color: #7ECBB5}")),
-                  tags$style(HTML(".js-irs-1 .irs-single, .js-irs-1 .irs-bar-edge, 
-                                  .js-irs-1 .irs-bar {color: #000; background: #FFDFA4 ; border-color: #FFDFA4  }")),
-                  tags$style(HTML(".js-irs-2 .irs-single, .js-irs-2 .irs-bar-edge, 
-                  .js-irs-2 .irs-bar {color: #000; background: #EBAF99 ; border-color: #EBAF99}")),
                   
-                
-                  tags$style(HTML(".js-irs-3 .irs-single, .js-irs-3 .irs-bar-edge, 
-                                  .js-irs-3 .irs-bar {color: #000; background: #c3c6c9 ; border-color: #c3c6c9  }")),
-                  tags$style(HTML(".js-irs-4 .irs-single, .js-irs-4 .irs-bar-edge, 
-                                  .js-irs-4 .irs-bar {color: #000; background: #c3c6c9 ; border-color: #c3c6c9  }")),
-                  tags$head(tags$style(".progress-bar{color: #000; background-color:#FFDFA4; border-color:: #fff; }")),
-                  tags$style(".nav-tabs-custom .nav-tabs li.active:hover a, .nav-tabs-custom .nav-tabs li.active a {colour: #000; background-color: #ffff2; border-color:: #fff;}"),
+                  tags$style(HTML(".js-irs-0 .irs-single, .js-irs-0 .irs-bar-edge,
+                  .js-irs-0 .irs-bar 
+                                  {color: #000; background: #c3c6c9 ; border-color: #9BA0A5}")),
+                  tags$style(HTML(".js-irs-1 .irs-single, .js-irs-1 .irs-bar-edge,
+                                  .js-irs-1 .irs-bar 
+                                  {color: #000; background: #c3c6c9 ; border-color: #9BA0A5}")),
+                  tags$style(HTML(".js-irs-2 .irs-single, .js-irs-2 .irs-bar-edge,
+                  .js-irs-2 .irs-bar 
+                                  {color: #000; background: #c3c6c9; border-color: #9BA0A5}")),
+
+                  tags$style(HTML(".js-irs-3 .irs-single, .js-irs-3 .irs-bar-edge,
+                                  .js-irs-3 .irs-bar 
+                                  {color: #000; background: #c3c6c9 ; border-color: #9BA0A5}")),
+                  tags$style(HTML(".js-irs-4 .irs-single, .js-irs-4 .irs-bar-edge,
+                                  .js-irs-4 .irs-bar 
+                                  {color: #000; background: #c3c6c9 ; border-color: #9BA0A5}")),
+
+                  tags$style(HTML(".js-irs-5 .irs-single, .js-irs-5 .irs-bar-edge,
+                                  .js-irs-5 .irs-bar 
+                                  {color: #000; background: #c3c6c9 ; border-color: #9BA0A5}")),
+                  tags$style(HTML(".js-irs-6 .irs-single, .js-irs-6 .irs-bar-edge,
+                                  .js-irs-6 .irs-bar 
+                                  {color: #000; background: #c3c6c9 ; border-color: #9BA0A5}")),
                   
+                  tags$style(HTML(".js-irs-7 .irs-single, .js-irs-7 .irs-bar-edge,
+                                  .js-irs-7 .irs-bar 
+                                  {color: #000; background: #c3c6c9 ; border-color: #9BA0A5}")),
 
                   
-  titlePanel("XMR Chart Generator"),
-  
+                  tags$head(tags$style(".progress-bar{color: #000; background-color: #EBAF99; border-color:#fff; }")),
+
+                  tags$style(HTML(".nav-tabs>li.active>a, .nav-tabs>li.active>a:focus, .nav-tabs>li.active>a:hover {background-color: #FFDFA4; border-color: #c3c6c9; color: #000;}")),
+                  
+                  
+                  
+                  tags$style(HTML(".nav-tabs > li > a {background-color: #c3c6c9; border-color:#c3c6c9; color: #000;}")),
+                  tags$style(".btn-file {colour: #b5e1d4 ; border-color: #fff2; background: #b5e1d4}"),
+
+                  #irs-slider single
+
+                  HTML("<br/>"),
+  img(src='logo.png', align = "center", width = "400px", alt = "Found me!"),
+  titlePanel("XMR Chart Builder"),
           sidebarPanel(fluid = TRUE,
                        tabsetPanel(
+
                         tabPanel("Upload",
-                           HTML("<br/>"),
-                           helpText("This program will take in your data and output a chart and plot 
-                                    with XMR boundaries calculated."),
-                           HTML("<b> Instructions </b>"),
-                           HTML("<br/>"),
-                           HTML("<b>1.</b> Upload a .csv file."),
-                           HTML("<br/>"),
-                           HTML("<b>2.</b> The first column of this .csv must be labeled 'Time', 
-                                and it must contain unique measures of time."),
-                           HTML("<br/>"),
-                           HTML("<b>3.</b> The second column must be labeled 'Measure', 
-                                and must contain some numeric measure."),
-                           HTML("<br/>"),
-                           HTML("<br/>"),
 
+                           helpText("This program takes in time-series data and outputs both a table and a plot
+                                    with calculated XMR boundaries."),
+                           HTML("<b> Instructions: </b>"),
+                           HTML("<br/>"),
+                           HTML("<b>1.</b> Upload a file."),
+                           HTML("<br/>"),
+                           HTML("<b>2.</b> One column must be labeled 'Time', 
+                                and it must contain unique and ordered measures of time."),
+                           HTML("<br/>"),
+                           HTML("<b>3.</b> Another column must be labeled 'Measure', 
+                                and must contain some numeric measure. This will be used for calculations."),
+                           # HTML("<br/>"),
+                           # HTML("<b>4.</b> Choose the correct file type."),
+                           HTML("<br/>"),
+                           HTML("<br/>"),
+                           radioButtons("type", "Choose Type of File:", choices = c(".csv", ".xlsx"), 
+                                        inline = F),
+                           fileInput("inputFile", NULL, buttonLabel = "Upload a file",
+                                     placeholder = "   No file uploaded", accept = "text/csv"),
+                           helpText("If you see error messages, ensure the file type is correct and your columns are labeled properly.")),
 
-                           fileInput("inputFile", NULL, buttonLabel = "Upload .csv file", width = "100%",
-                                     placeholder = "No file uploaded")),
-                        tabPanel("Chart Properties",
-                          HTML("<br/>"),
+                        tabPanel("Properties",
+                          #HTML("<br/>"),
                           helpText("Label your chart and choose it's properties"),
-                           textInput("title", label = "Chart Title", value = ""),
-                           textInput("x", label = "X-Axis Label", value = "Time"),
-                           textInput("y", label = "Y-Axis Label", value = "Measure"),
-                          numericInput("titlesize", "Chart Title Size", min = 5, max = 50, value = 20),
-                          numericInput("textsize", "Axis Label Size", min = 5, max = 20, value = 15),
-
-                          sliderInput("dotsize", "Point Size", value = 5, min = 3, max = 20, ticks = F),
-                          sliderInput("labelsize", "Label Size", value = 6, min = 3, max = 20, ticks = F),
-                          sliderInput("refline", "Reference Line Size", value = 1, min = 0, max = 2, 
-                                      ticks = F, step = 0.1)),
-                       
-
+                           textInput("title", label = "Chart Title:", value = ""),
+                          textInput("subtitle", label = "Subtitle:", value = ""),
+                          radioButtons("center", "Title Position:", choices = c("Left", "Center"), 
+                                       inline = T),
+                           textInput("x", label = "X-Axis Title:", value = ""),
+                           textInput("y", label = "Y-Axis Title:", value = ""),
+                          checkboxInput("grid", "Grid Lines", value = TRUE),
+                          sliderInput("titlesize", "Chart Title Size:", min = 5, max = 50, 
+                                      value = 20, ticks = F, step = 0.1),
+                          sliderInput("textsize", "Axis Text Size:", min = 5, max = 20, 
+                                      value = 15, ticks = F, step = 0.1),
+                          sliderInput("labelsize", "Label Size:", value = 6, min = 0, max = 20, 
+                                      ticks = F, step = 0.1),
+                          radioButtons("l_format", "Label Format:", 
+                                       choices = c("Raw", "Percent", "Dollars"), inline = T),
+                          numericInput("digits", "Label Digits:", value = 2, min = 0, max = 5),
+                          sliderInput("dotsize", "Point Size:", value = 5, min = 3, max = 20, 
+                                      ticks = F, step = 0.1),
+                          sliderInput("refline", "Reference Line Size:", value = 1, min = 0, max = 2, 
+                                      ticks = F, step = 0.01),
+                          colourInput("dotcol", "Point Colour:", value = "#7ECBB5"),
+                          colourInput("linecol", "Reference Line Colour:", value = "#d02b27")
+                        ),
+                      
                         tabPanel("Export",
-                           HTML("<br/>"),
-                           helpText("Save the XMR plots using these button."),
-                           helpText("PNG can be placed into Excel."),
+                           #HTML("<br/>"),
+                           helpText("Save the XMR charts using the button below"),
+                           helpText("PNGs can be easily placed into Excel or Word."),
                            
                            downloadButton('downloadPlot', 'Save PDF', 
-                                          style ="color: #000; background-color: #7ECBB5; border-color: #7ECBB5"),
+                                          style ="color: #000; background-color: #b5e1d4; border-color: #b5e1d4"),
                            downloadButton('downloadPlotPNG', 'Save PNG', 
-                                          style ="color: #000; background-color: #7ECBB5; border-color: #7ECBB5"),
+                                          style ="color: #000; background-color: #b5e1d4; border-color: #b5e1d4"),
                            helpText(""),
                            #radioButtons("type", "PDF or PNG", select = "PDF", choices = c("PDF", "PNG")),
-                           sliderInput("height", "Saved Plot Height (inches)", value = 4, min = 3, max = 20, ticks = F),
-                           sliderInput("width", "Saved Plot Width (inches)", value = 14, min = 3, max = 20, ticks = F),
+                           sliderInput("height", "Saved Chart Height (inches)", 
+                                       value = 4, min = 3, max = 20, ticks = F),
+                           sliderInput("width", "Saved Chart Width (inches)", 
+                                       value = 14, min = 3, max = 20, ticks = F),
+                           sliderInput("dpi", "Saved Chart Resolution (DPI)", 
+                                       value = 600, min = 150, max = 1600, ticks = F),
                            helpText("Save the table to make your own XMR charts."),
                            downloadButton('downloadData', 'Save Table', 
                                           style ="color: #000; background-color: #EBAF99; border-color: #EBAF99"),
@@ -78,8 +126,9 @@ shinyUI(fluidPage(theme = "www/paper_modified.css",
                        )),
                        width = 3),
 
-
-  mainPanel(plotOutput("xmr", width = "auto"),  
+  
+  mainPanel(
+            plotOutput("xmr", width = "auto"),  
             HTML("<br/>"),
             tableOutput("contents"))
   )
